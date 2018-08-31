a95d4bc2d060146375d8a4e18c7484a576a9583a
Tam Nguyen <tam.nguyen676@gmail.com>
2018-08-31 11:51:58 -0500

    Fixed bugs in GPS script

--- QPython/gps.py
+++ QPython/gps.py
@@ -1,18 +1,20 @@
 import android, time
 droid = android.Android()
 droid.startLocating()
-  
+
 while True:
  time.sleep(5)
  loc = droid.readLocation().result
- if loc = {}:
-   loc = getLastKnownLocation().result
+ if loc == {}:
+   print("Can't get current location, getting last known location")
+   loc = droid.getLastKnownLocation().result
  if loc != {}:
    try:
      n = loc['gps']
    except KeyError:
+     print("Can't get GPS location, using network")
      n = loc['network'] 
    la = n['latitude'] 
    lo = n['longitude']
    address = droid.geocode(la, lo).result
-   print("lat: {0}, long: {1}".format(la, lo)
+   print("lat: {0}, long: {1}".format(la, lo))
